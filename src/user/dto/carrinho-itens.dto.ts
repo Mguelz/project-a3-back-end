d2e724c4ce7ad5fb1e82517b1a7db9d1dae36536
Miguel <945507756m@gmail.com>
2024-05-31 17:27:44 -0300

    crud itens carinho funcionando

--- src/user/dto/carrinho-itens.dto.ts
+++ src/user/dto/carrinho-itens.dto.ts
@@ -2,32 +2,29 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsNumber, Min } from 'class-validator';
 
 export class CreateCarrinhoItensDto {
-  @IsNumber()
-  carrinhoIdCarrinho: number;
-
-  @IsNumber()
-  catalogoIdCatalogo: number;
-
   @IsNumber(
     { maxDecimalPlaces: 2 },
     { message: 'O desconto deve ser um número com no máximo 2 casas decimais' },
   )
   @Min(0, { message: 'O desconto deve ser maior ou igual a 0' })
   desconto?: number;
-
+  
   @IsNumber(
     { maxDecimalPlaces: 2 },
     {
       message:
-        'O preço do item deve ser um número com no máximo 2 casas decimais',
+      'O preço do item deve ser um número com no máximo 2 casas decimais',
     },
   )
   @Min(0, { message: 'O preço do item deve ser maior ou igual a 0' })
   preco_item: number;
-
+  
   @IsNumber()
   @Min(1, { message: 'A quantidade deve ser maior ou igual a 1' })
   quantidade: number;
+
+  @IsNumber()
+  catalogoIdCatalogo: number;
 }
 
 export class UpdateCarrinhoItensDto extends PartialType(
--- src/user/service/carrinho-itens.service.ts
+++ src/user/service/carrinho-itens.service.ts
@@ -3,7 +3,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CarrinhoItensEntity } from '../entity/carrinho-itens.entity';
 import { CarrinhoCabecaService } from './carrinho-cabeca.service';
-import { CreateCarrinhoItensDto, UpdateCarrinhoItensDto } from '../dto/carrinho-itens.dto';
+import {
+  CreateCarrinhoItensDto,
+  UpdateCarrinhoItensDto,
+} from '../dto/carrinho-itens.dto';
 import { CatalogoService } from './catalogo-cabeca.service';
 
 @Injectable()
@@ -15,13 +18,12 @@ export class CarrinhoItensService {
     private catalogoService: CatalogoService,
   ) {}
 
-  async create(createCarrinhoItensDto: CreateCarrinhoItensDto): Promise<CarrinhoItensEntity> {
-    const carrinho = await this.carrinhoCabecaService.findOne(createCarrinhoItensDto.carrinhoIdCarrinho);
-    const catalogo = await this.catalogoService.findOne(createCarrinhoItensDto.catalogoIdCatalogo);
-
-    if (!carrinho) {
-      throw new NotFoundException(`Carrinho não encontrado.`);
-    }
+  async create(
+    createCarrinhoItensDto: CreateCarrinhoItensDto,
+  ): Promise<CarrinhoItensEntity> {
+    const catalogo = await this.catalogoService.findOne(
+      createCarrinhoItensDto.catalogoIdCatalogo,
+    );
 
     if (!catalogo) {
       throw new NotFoundException(`Catálogo não encontrado.`);
@@ -29,14 +31,15 @@ export class CarrinhoItensService {
 
     const newCarrinhoItens = this.carrinhoItensRepository.create({
       ...createCarrinhoItensDto,
-      carrinho: carrinho,
       catalogo: catalogo,
     });
     return await this.carrinhoItensRepository.save(newCarrinhoItens);
   }
 
   async findAll(): Promise<CarrinhoItensEntity[]> {
-    return await this.carrinhoItensRepository.find({ relations: ['carrinho', 'catalogo'] });
+    return await this.carrinhoItensRepository.find({
+      relations: ['carrinho', 'catalogo'],
+    });
   }
 
   async findOne(id: number): Promise<CarrinhoItensEntity> {
@@ -50,26 +53,26 @@ export class CarrinhoItensService {
     return carrinhoItem;
   }
 
-  async update(id: number, updateCarrinhoItensDto: UpdateCarrinhoItensDto): Promise<CarrinhoItensEntity> {
+  async update(
+    id: number,
+    updateCarrinhoItensDto: UpdateCarrinhoItensDto,
+  ): Promise<CarrinhoItensEntity> {
     const carrinhoItem = await this.findOne(id);
 
-    if (updateCarrinhoItensDto.carrinhoIdCarrinho) {
-      const carrinho = await this.carrinhoCabecaService.findOne(updateCarrinhoItensDto.carrinhoIdCarrinho);
-      if (!carrinho) {
-        throw new NotFoundException(`Carrinho não encontrado.`);
-      }
-      carrinhoItem.carrinho = carrinho;
-    }
-
     if (updateCarrinhoItensDto.catalogoIdCatalogo) {
-      const catalogo = await this.catalogoService.findOne(updateCarrinhoItensDto.catalogoIdCatalogo);
+      const catalogo = await this.catalogoService.findOne(
+        updateCarrinhoItensDto.catalogoIdCatalogo,
+      );
       if (!catalogo) {
         throw new NotFoundException(`Catálogo não encontrado.`);
       }
       carrinhoItem.catalogo = catalogo;
     }
 
-    const updatedCarrinhoItens = this.carrinhoItensRepository.merge(carrinhoItem, updateCarrinhoItensDto);
+    const updatedCarrinhoItens = this.carrinhoItensRepository.merge(
+      carrinhoItem,
+      updateCarrinhoItensDto,
+    );
     return await this.carrinhoItensRepository.save(updatedCarrinhoItens);
   }
 
@@ -78,80 +81,3 @@ export class CarrinhoItensService {
     await this.carrinhoItensRepository.remove(carrinhoItem);
   }
 }
-
-
-// import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { CarrinhoItensEntity } from '../entity/carrinho-itens.entity';
-// import {
-//   CreateCarrinhoItensDto,
-//   UpdateCarrinhoItensDto,
-// } from '../dto/carrinho-itens.dto';
-
-// @Injectable()
-// export class CarrinhoItensService {
-//   constructor(
-//     @InjectRepository(CarrinhoItensEntity)
-//     private carrinhoItensRepository: Repository<CarrinhoItensEntity>,
-//   ) {}
-
-//   async findAll(): Promise<CarrinhoItensEntity[]> {
-//     return await this.carrinhoItensRepository.find({
-//       relations: ['carrinhos', 'catalogos'],
-//     });
-//   }
-
-//   async findOne(id: number): Promise<CarrinhoItensEntity> {
-//     const itens = await this.carrinhoItensRepository.findOne({
-//       where: { id_carrinho_item: id },
-//       relations: ['carrinhos', 'catalogos'],
-//     });
-//     if (!itens) {
-//       throw new HttpException(`Carrinho não encontrado.`, HttpStatus.NOT_FOUND);
-//     }
-//     return itens;
-//   }
-
-//   async create(
-//     createCarrinhoItensDto: CreateCarrinhoItensDto,
-//   ): Promise<CarrinhoItensEntity> {
-//     try {
-//       return await this.carrinhoItensRepository.save(
-//         this.carrinhoItensRepository.create(createCarrinhoItensDto),
-//       );
-//     } catch (error) {
-//       if (error.code === 'ER_DUP_ENTRY') {
-//         throw new HttpException(
-//           'Há registros repetidos.',
-//           HttpStatus.BAD_REQUEST,
-//         );
-//       } else {
-//         throw new HttpException(
-//           'Erro ao criar o carrinho.',
-//           HttpStatus.INTERNAL_SERVER_ERROR,
-//         );
-//       }
-//     }
-//   }
-
-//   async update(
-//     id: number,
-//     updateCarrinhoItensDto: UpdateCarrinhoItensDto,
-//   ): Promise<void> {
-//     const result = await this.carrinhoItensRepository.update(
-//       id,
-//       updateCarrinhoItensDto,
-//     );
-//     if (result.affected === 0) {
-//       throw new HttpException(`Carrinho não encontrado.`, HttpStatus.NOT_FOUND);
-//     }
-//   }
-
-//   async delete(id: number): Promise<void> {
-//     const result = await this.carrinhoItensRepository.delete(id);
-//     if (result.affected === 0) {
-//       throw new HttpException(`Carrinho não encontrado.`, HttpStatus.NOT_FOUND);
-//     }
-//   }
-// }
